4f8502340e9087e03aad905604e3a0a21f7889fd
adodriano <adodriano@gmail.com>
2024-04-03 02:19:18 -0300

    Add project files.

---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,30 @@
+﻿using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventUseCase
+    {
+        public void Execute(RequestEventJson request)
+        {
+            Validate(request);
+        }
+
+        public void Validate(RequestEventJson request)
+        {
+            if (request.MaximumAttendees <= 0)
+            {
+                throw new ArgumentNullException("The Maximum atendees is invalid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Title))
+            {
+                throw new ArgumentNullException("The title is invalid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Details))
+            {
+                throw new ArgumentNullException("The Details is invalid.");
+            }
+        }
+    }
+}
